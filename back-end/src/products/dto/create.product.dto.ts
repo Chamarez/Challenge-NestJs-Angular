c95e37b3257d91fe415ca525b851250b25427340
Michel-Chamarez <michel-chamarez@Imutes.wind3.hub>
2022-07-29 23:05:01 +0200

    feat: validations and pipe

--- back-end/src/products/dto/create.product.dto.ts
+++ back-end/src/products/dto/create.product.dto.ts
@@ -1,8 +1,22 @@
+import { IsString, IsInt, IsNumber, IsNotEmpty } from 'class-validator';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   readonly title: string;
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+  @IsNotEmpty()
+  @IsNumber()
   readonly price: number;
+  @IsNotEmpty()
+  @IsNumber()
   readonly stock: number;
+  @IsNotEmpty()
+  @IsNumber()
   readonly weight: number;
+  @IsNotEmpty()
+  @IsNumber()
   readonly size: number;
 }
