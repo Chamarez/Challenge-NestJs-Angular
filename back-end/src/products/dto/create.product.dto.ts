230f1e5c891f16609798b66a797e5ae52c0cc72e
Michel-Chamarez <michel-chamarez@Imutes.wind3.hub>
2022-07-29 22:50:34 +0200

    feat: crud implemented

---
+++ back-end/src/products/dto/create.product.dto.ts
@@ -0,0 +1,8 @@
+export class CreateProductDto {
+  readonly title: string;
+  readonly description: string;
+  readonly price: number;
+  readonly stock: number;
+  readonly weight: number;
+  readonly size: number;
+}
